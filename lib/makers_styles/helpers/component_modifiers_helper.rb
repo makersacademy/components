5be907b02be9d3e8310360bacd84536cf74a4207
Sam Morgan <s_morgan@me.com>
2015-05-18 17:19:22 +0100

    partialized cta-block

--- lib/makers_styles/helpers/component_modifiers_helper.rb
+++ lib/makers_styles/helpers/component_modifiers_helper.rb
@@ -1,5 +1,5 @@
 module ComponentModifiersHelper
   def component_modifiers component, modifiers
-    modifiers.inject("#{component}") { | str, mod | str << " cta--#{mod}" }
+    modifiers.inject("#{component}") { | str, mod | str << " #{component}--#{mod}" }
   end
 end
