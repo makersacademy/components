ffbfe155aa05695a19cfd84ff1999da9c6f56ee8
Sam Morgan <s_morgan@me.com>
2015-05-20 14:24:24 +0100

    streamlined ComponentClassesHelper a bit

--- lib/makers_styles/helpers/component_classes_helper.rb
+++ lib/makers_styles/helpers/component_classes_helper.rb
@@ -1,7 +1,21 @@
 module ComponentClassesHelper
   def component_classes parent=nil, component="", modifiers=[]
-    modifier_classes = modifiers.inject("#{component}") { | str, mod | str << " #{component}--#{mod}" }
-    block_classes = parent ? "#{parent}__#{component}" : ""
-    (modifier_classes + " " + block_classes).rstrip
+    modifier_classes = construct_modifier_classes component, modifiers
+    block_classes = construct_block_classes parent, component
+    combine_classes modifier_classes, block_classes
+  end
+
+  private
+
+  def construct_modifier_classes component, modifiers
+    modifiers.inject("#{component}") { | str, mod | str << " #{component}--#{mod}" }
+  end
+
+  def construct_block_classes parent, component
+    parent ? "#{parent}__#{component}" : ""
+  end
+
+  def combine_classes modifier_classes, block_classes
+    "#{modifier_classes} #{block_classes}".rstrip
   end
 end
