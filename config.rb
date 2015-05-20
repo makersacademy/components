3645da2ea4d976f27695b497a2795165bb283acd
Leo Allen <leo.f.allen@gmail.com>
2015-05-20 20:04:44 +0100

    Builds and deploys automatically to github pages

--- config.rb
+++ config.rb
@@ -72,18 +72,15 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+   #For example, change the Compass output style for deployment
+   activate :minify_css
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+   #Minify Javascript on build
+   activate :minify_javascript
 
-  # Enable cache buster
-  # activate :asset_hash
+   #Enable cache buster
+   activate :asset_hash
 
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+   #Use relative URLs
+   activate :relative_assets
 end
