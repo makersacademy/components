b75a1dc953475186c218c618d9834e5c81aadabd
Sam Morgan <s_morgan@me.com>
2015-05-20 14:17:49 +0100

    removes unnecessary test for CaptionNewlinesToCaptionBodiesHelper

--- lib/makers_styles/helpers/component_classes_helper.rb
+++ lib/makers_styles/helpers/component_classes_helper.rb
@@ -1,10 +1,5 @@
 module ComponentClassesHelper
-  def component_modifiers component, modifiers
-    modifiers.inject("#{component}") { | str, mod | str << " #{component}--#{mod}" }
-  end
-
   def component_classes parent=nil, component="", modifiers=[]
-    modifiers ||= []
     modifier_classes = modifiers.inject("#{component}") { | str, mod | str << " #{component}--#{mod}" }
     block_classes = parent ? "#{parent}__#{component}" : ""
     (modifier_classes + " " + block_classes).rstrip
