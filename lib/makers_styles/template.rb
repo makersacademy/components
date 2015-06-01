a4e0a044e1ca074ca7073c75a4d9fe1cc4197761
Leo Allen <leo.f.allen@gmail.com>
2015-06-01 14:08:10 +0100

    Loads template from file

--- lib/makers_styles/template.rb
+++ lib/makers_styles/template.rb
@@ -1,8 +1,15 @@
 require "hashie/mash"
+require "yaml"
 
 # This class has to subclass from hashie as we need templates to
 # work with Sinatra's partial helper - this requires that all keys are symbols
 # but Ruby's yaml parser passes them in as strings annoyingly
 module MakersStyles
-  class Template < Hashie::Mash; end
+  class Template < Hashie::Mash
+
+    def self.from_file(path)
+      content = YAML.load_file(path)
+      new(content)
+    end
+  end
 end
--- spec/makers_styles/template_spec.rb
+++ spec/makers_styles/template_spec.rb
@@ -17,5 +17,16 @@ module MakersStyles
     it "has templates that can be passed to a partial" do
       expect(subject[:text][:primary]).to eq("nav")
     end
+
+    describe "Loading a template from file" do
+      before do
+        allow(YAML).to receive(:load_file).with("file.yml").and_return(data)
+        allow(described_class).to receive(:new).with(data).and_return(:template)
+      end
+
+      it "parses a YAML file and creates a new template" do
+        expect(described_class.from_file("file.yml")).to eq(:template)
+      end
+    end
   end
 end
